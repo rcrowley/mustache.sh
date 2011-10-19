473539e1c8085861f3baead91d45adb1a3f2f13c
Richard Crowley <r@rcrowley.org>
2011-10-19 17:34:17 +0000

    Document test case.

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
 set -e
 
-. lib/mustache.sh
+# Source the `mustache.sh` library.
+. "lib/mustache.sh"
 
+# Set some variables.  They don't need to be exported to be made available
+# to the `mustache` function.
 FOO="foo" BAR="bar"
 
+# Call the `mustache` function, passing a template on standard input and
+# diffing standard output against a known-good copy.
 mustache <"test.mustache" | diff -u - "test.mustache.out"
